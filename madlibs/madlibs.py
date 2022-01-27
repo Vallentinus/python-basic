3dcb3bd225b066b30ebfeca0cb7eacecb9c23b58
Vallentinus <84045751+Vallentinus@users.noreply.github.com>
2022-01-27 19:53:24 +0300

    Add files via upload

---
+++ madlibs/madlibs.py
@@ -0,0 +1,10 @@
+adj = input("Adjective:")
+verb1 = input("Verb1:")
+verb2 = input("Verb2:")
+famous_person = input("Famous person:")
+
+
+madlib = f"Life is so {adj}. It makes me so {adj} all the time becouse \
+I'm {verb1}. Stay calm and {verb2} you're like {famous_person}"
+
+print(madlib)
