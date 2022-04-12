35c32688f44b1680c92735d933644c09fd6c89ee
Kerem <84045751+Vallentinus@users.noreply.github.com>
2022-04-12 21:46:20 +0300

    Add files via upload

---
+++ rocket on terminal/rocket.py
@@ -0,0 +1,26 @@
+from time import sleep
+ 
+def printRocket():
+    print(
+"""
+           _
+          /^\\
+          |-|
+          | |
+          |N|
+          |A|
+          |S|
+          |A|
+         /| |\\
+        / | | \\
+       |  | |  |
+       `-\\"\\"\\"-`
+""")
+    
+printRocket()
+ 
+delay = 300
+for i in range(60):
+    print()
+    sleep(delay/1000)
+    delay = delay * 0.9
